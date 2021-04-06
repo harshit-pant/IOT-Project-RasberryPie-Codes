d741b2a73ec33b6b8b80b76272b01326c8253852
Harshit Pant <30293858+harshit-pant@users.noreply.github.com>
2021-04-06 18:18:21 +0530

    Update rasberrypicode.py

--- rasberrypicode.py
+++ rasberrypicode.py
@@ -1,49 +1,47 @@
+# importing lib
 import RPi.GPIO as GPIO
 from time import sleep
-import httplib2
+from datetime import datetime
+from firebase import firebase
 import json
-import urllib.request
-import os
-import jwt
-import pyrebase
 
+
+
+# setting up raspberry pi GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 
- config = {
+#connecting raspiberry pi to firebase
+firebase = firebase.FirebaseApplication('https://arduinofirebaseconnectio-d58e9-default-rtdb.firebaseio.com/', None)
 
-  "apiKey": "JQTcsq6HNfJlZyx30iPJOCOw93YHI1AE8QRyBnBv",
-
-  "authDomain": "arduinofirebaseconnectio-d58e9.firebaseapp.com",
-
-  "databaseURL": "https://arduinofirebaseconnectio-d58e9-default-rtdb .firebaseio.com",
-
-  "storageBucket": "arduinofirebaseconnectio-d58e9.appspot.com",
-
- # "serviceAccount": "path/to/serviceAccountCredentials.json"
-
-}
-firebase = pyrebase.initialize_app(config)
-
+#setting the input pin(4) Real: PIN-7
 GPIO.setup(4,GPIO.IN)
 
 
+#function to send data
 def update_firebase():
 
 	moisture= GPIO.input(4)
+	print(moisture)
 	if moisture is not None:
+		print("Sending the data......")
+		now = datetime.now()
+		current_time = now.strftime("%H:%M:%S")
 		
-		firebase.post(moisture)			
+		z = {
+			"Name": "Harshit",
+			"now":current_time,
+			"Moisture": moisture
+
+		    }
+		firebase.post('str',z)			
 			
 	else:
 		print('Failed to get reading. Try again!')	
 		sleep(10)
 
 	
-	
-
-	while True:
-		update_firebase()
-		sleep(5)
-
+while True:
+	update_firebase()
+	sleep(5)
